939dd0a1543a3a8b2cb65726d0f9d922847d73e2
i16fujimoto <fujimoto.ryusei@arakawa-lab.com>
2024-02-13 09:49:34 +0900

    feat: add nginx

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -10,18 +10,22 @@ resource "aws_ecr_repository" "main" {
   tags                 = merge(var.tags, { "Name" = "ecr-fargate-cicd" })
 }
 
+# NOTE: 同じECRリポジトリで使用できるaws_ecr_lifecycle_policyリソースは1つだけです。複数のルールを適用するには、ポリシーJSONで組み合わせる必要があります。
 resource "aws_ecr_lifecycle_policy" "main" {
   repository = aws_ecr_repository.main.name
   policy = jsonencode({
     rules = [
       {
+        # NOTE: AWS ECR APIはrulePriorityに基づいてルールを並び替えるようです。TerraformのコードでrulePriorityの昇順でソートされていないルールを複数定義した場合、リソースはterraformのプランごとに再作成のフラグが立ってしまう。
         rulePriority = 1,
         description  = "keep last 10 images"
+        # NOTE: アクション・タイプを指定する。サポートされている値はexpireです。
         action = {
           type = "expire"
         }
         selection = {
-          tagStatus = "any"
+          tagStatus = "any" # タグが付いている or ついていない or どちらでも を指定
+          # countType を sinceImagePushed に設定した場合は、countUnit と countNumber も指定して、リポジトリに存在するイメージの時間制限を指定します。
           countType = "imageCountMoreThan"
           countNumber = 10
         },
